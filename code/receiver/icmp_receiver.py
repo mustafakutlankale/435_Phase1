11c9722bbdfbb2fc2a9e89525cc84a06d3341007
Mustafa Kutlan Kale <88057509+mustafakutlankale@users.noreply.github.com>
2024-11-01 00:54:57 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,19 +1,16 @@
 from scapy.all import *
 
 def receive_packet(packet):
-    # Check if the packet has an ICMP layer
     if packet.haslayer(ICMP):
         icmp_layer = packet[ICMP]
         ip_layer = packet[IP]
 
-        # Print only the incoming ICMP request with TTL=1
-        if ip_layer.ttl == 1 and icmp_layer.type == 8:  # Type 8 indicates an ICMP Echo Request
+        if ip_layer.ttl == 1 and icmp_layer.type == 8:  
             print("Received ICMP request packet:")
-            icmp_layer.show()  # Display detailed information about the packet
+            icmp_layer.show()  
 
 def main():
     print("Listening for ICMP packet...")
-    #sniff(filter="icmp", prn=receive_packet)
     t = AsyncSniffer(filter="icmp", prn=receive_packet, count=1)
     t.start()
     t.join() 
