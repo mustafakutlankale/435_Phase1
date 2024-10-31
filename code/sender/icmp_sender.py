a7b175fdfe352c052a332b5b76dfd5b339feac8f
Mustafa Kutlan Kale <mustafa.kale@metu.edu.tr>
2024-10-31 23:57:48 +0300

    Implemented send_icmp_request method

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,17 @@
-import scapy
+from scapy.all import *
 
 # Implement your ICMP sender here
+def send_icmp_request(target_ip):
+    icmp_packet = IP(dst=target_ip)/ICMP()
+
+    sr1(icmp_packet, ttl=1)
+
+if __name__ == "__main__":
+    target_ip = "receiver"
+    send_icmp_request(target_ip)
+
+
+
+
+
+
