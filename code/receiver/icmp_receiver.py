bcc6a3780930004cd04b9f5a507763b121e930d2
hknUcak <115832254+hknUcak@users.noreply.github.com>
2024-11-02 22:27:34 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,4 @@
+# Implement your ICMP receiver here
 from scapy.all import *
 
 def receive_packet(packet):
@@ -5,15 +6,10 @@ def receive_packet(packet):
         icmp_layer = packet[ICMP]
         ip_layer = packet[IP]
 
-        if ip_layer.ttl == 1 and icmp_layer.type == 8:  
-            print("Received ICMP request packet:")
+        if ip_layer.ttl == 1 and icmp_layer.type == 8:
             icmp_layer.show()  
 
-def main():
-    print("Listening for ICMP packet...")
-    t = AsyncSniffer(filter="icmp", prn=receive_packet, count=1)
-    t.start()
-    t.join() 
 
-if __name__ == "__main__":
-    main()
+t = AsyncSniffer(filter="icmp", prn=receive_packet, count=1)
+t.start()
+t.join() 
